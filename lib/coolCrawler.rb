0aae774bdebb8e52d04555c3117bad1f8df6037e
William Wright <williamwright3@cmail.carleton.ca>
2022-09-26 12:27:56 -0400

    first

--- lib/coolCrawler.rb
+++ lib/coolCrawler.rb
@@ -4,5 +4,32 @@ require_relative "coolCrawler/version"
 
 module CoolCrawler
   class Error < StandardError; end
-  # Your code goes here...
+
+
+  class CrawlerPool
+
+    def initialize(threads, delay)
+      @threads = threads
+      @delay = delay
+    end
+
+  end
+
+  class Crawler
+
+    def initialize
+    end
+
+    # Can be override with 
+    def after
+    end
+
+    def bfs
+    end
+
+
+
+
+  end
+
 end
